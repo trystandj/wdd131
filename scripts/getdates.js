06d790a841887f1e2fa7ed4fa6ab5a94c129ba0c
trystandj <trystandj@gmail.com>
2025-01-10 21:54:19 -0500

    JS added to get the last modified date and time

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+const lastModified = document.querySelector("#lastModified");
+
+let oLastModif = new Date(document.lastModified);
+
+lastModified.innerHTML = `<span class="highlight">Last Modification: ${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full",
+        timeStyle: "short",
+	}
+).format(oLastModif)}</span>`;
