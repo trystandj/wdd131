f413ea2cfb6b5dee8ea7307dfd744c175b3a896b
trystandj <trystandj@gmail.com>
2025-01-15 18:46:37 -0500

    finished debugging

--- week02/js_Practice/debugging.js
+++ week02/js_Practice/debugging.js
@@ -1,6 +1,9 @@
 const PI = 3.14;
-let radius = 3;
 let area = 0;
-area = radius * radius *PI;
-radius = 4;
-area = radius * radius *PI;
+
+function circleArea(radius) {
+  const area = radius * radius *PI;
+    return area;
+}
+area = circleArea(3);
+console.log(area);
