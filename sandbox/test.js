276f39e0b3fbe04478ed6c34093992f8cc2676b9
trystandj <trystandj@gmail.com>
2025-01-23 08:21:42 -0500

    js

--- sandbox/test.js
+++ sandbox/test.js
@@ -5,3 +5,30 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+
+let firstName = "antonia";
+let lastName = "francesca";
+
+function fullName(firstName, lastName) {
+	return firstName + " " + lastName;
+}
+// or
+// function fullName(first, last) {
+// 	return `${first} ${last}`;   // string concatenation first + ' ' + last (not preferred)
+//   }
+// console.log(fullName(firstName, lastName));
+
+// const fullName = function (firstName, lastName) {
+// 	return firstName + " " + lastName;
+// }
+
+const fullName = (firstName, lastName) => ${firstName} ${lastName};
+
+document.querySelector('#fullName').innerHTML = fullName(firstName, lastName);
+
+false
+numbers.lastindex(2)
+
+"fasl=se"
